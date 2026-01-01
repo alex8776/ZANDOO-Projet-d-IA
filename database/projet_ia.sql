f720c241b30bb57234ed895620073a325e1b23d3
victoria <djoudjovictoria12@gmail.com>
2026-01-01 12:37:23 +0100

    Ajout du script SQL projet_ia

---
+++ database/projet_ia.sql
@@ -0,0 +1,258 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 01 jan. 2026 à 11:48
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet_ia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `idClient` int(11) NOT NULL,
+  `nomClient` varchar(100) NOT NULL,
+  `dateNaissance` date DEFAULT NULL,
+  `sexe` varchar(10) DEFAULT NULL,
+  `ville` varchar(100) DEFAULT NULL,
+  `photoProfil` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `idCommande` int(11) NOT NULL,
+  `idClient` int(11) NOT NULL,
+  `idProduit` int(11) NOT NULL,
+  `dateCommande` date DEFAULT NULL,
+  `quantite` int(11) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `preference`
+--
+
+CREATE TABLE `preference` (
+  `idClient` int(11) NOT NULL,
+  `idProduit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `idProduit` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL,
+  `qualite` varchar(50) DEFAULT NULL,
+  `prix` decimal(10,2) NOT NULL,
+  `categorie` varchar(100) DEFAULT NULL,
+  `datePublication` date DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `imageProduit` varchar(255) DEFAULT NULL,
+  `idVendeur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `profil_client`
+--
+
+CREATE TABLE `profil_client` (
+  `idProfilClient` int(11) NOT NULL,
+  `attribute1` varchar(100) DEFAULT NULL,
+  `attribute2` varchar(100) DEFAULT NULL,
+  `attribute3` varchar(100) DEFAULT NULL,
+  `idClient` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `profil_produit`
+--
+
+CREATE TABLE `profil_produit` (
+  `idProfilProduit` int(11) NOT NULL,
+  `attribute1` varchar(100) DEFAULT NULL,
+  `attribute2` varchar(100) DEFAULT NULL,
+  `attribute3` varchar(100) DEFAULT NULL,
+  `idProduit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vendeur`
+--
+
+CREATE TABLE `vendeur` (
+  `idVendeur` int(11) NOT NULL,
+  `nomVendeur` varchar(100) NOT NULL,
+  `prenomVendeur` varchar(100) DEFAULT NULL,
+  `dateNaissance` date DEFAULT NULL,
+  `ville` varchar(100) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `sexe` varchar(10) DEFAULT NULL,
+  `photoProfil` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`idClient`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`idCommande`),
+  ADD KEY `idClient` (`idClient`),
+  ADD KEY `idProduit` (`idProduit`);
+
+--
+-- Index pour la table `preference`
+--
+ALTER TABLE `preference`
+  ADD PRIMARY KEY (`idClient`,`idProduit`),
+  ADD KEY `idProduit` (`idProduit`);
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`idProduit`),
+  ADD KEY `idVendeur` (`idVendeur`);
+
+--
+-- Index pour la table `profil_client`
+--
+ALTER TABLE `profil_client`
+  ADD PRIMARY KEY (`idProfilClient`),
+  ADD KEY `idClient` (`idClient`);
+
+--
+-- Index pour la table `profil_produit`
+--
+ALTER TABLE `profil_produit`
+  ADD PRIMARY KEY (`idProfilProduit`),
+  ADD KEY `idProduit` (`idProduit`);
+
+--
+-- Index pour la table `vendeur`
+--
+ALTER TABLE `vendeur`
+  ADD PRIMARY KEY (`idVendeur`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `idClient` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `idCommande` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `idProduit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `profil_client`
+--
+ALTER TABLE `profil_client`
+  MODIFY `idProfilClient` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `profil_produit`
+--
+ALTER TABLE `profil_produit`
+  MODIFY `idProfilProduit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `vendeur`
+--
+ALTER TABLE `vendeur`
+  MODIFY `idVendeur` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `client` (`idClient`),
+  ADD CONSTRAINT `commande_ibfk_2` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`idProduit`);
+
+--
+-- Contraintes pour la table `preference`
+--
+ALTER TABLE `preference`
+  ADD CONSTRAINT `preference_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `client` (`idClient`),
+  ADD CONSTRAINT `preference_ibfk_2` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`idProduit`);
+
+--
+-- Contraintes pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`idVendeur`) REFERENCES `vendeur` (`idVendeur`);
+
+--
+-- Contraintes pour la table `profil_client`
+--
+ALTER TABLE `profil_client`
+  ADD CONSTRAINT `profil_client_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `client` (`idClient`);
+
+--
+-- Contraintes pour la table `profil_produit`
+--
+ALTER TABLE `profil_produit`
+  ADD CONSTRAINT `profil_produit_ibfk_1` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`idProduit`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
